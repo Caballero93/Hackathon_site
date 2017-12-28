ba7a292a291138977eff8e7ca1a6130bfa340959
Stefan <stefan.soic@typhoon-hil.ch>
2017-12-28 18:07:27 +0100

    Izbacen if name==main iz submit

--- typhoon/views.py
+++ typhoon/views.py
@@ -5,11 +5,12 @@ import os
 import subprocess
 import shutil
 import importlib.util
-from . models import HallOfFame
+from .models import HallOfFame
 
 
 def my_homepage_view(request):
     return render(request, "my_homepage_view.html")
+
 
 def submit_sol(request):
     error = False
@@ -20,39 +21,44 @@ def submit_sol(request):
         else:
             os.chdir("./hackathon")
             cwd = str(os.getcwd())
-            if __name__ == '__main__':
-                theirs = 'hackathon2017.their'
-                # os.mkdir(theirs)
-                # return HttpResponse(cwd)
-                os.system('git reset --hard')
-                subprocess.run(['git', 'clone', r, theirs])
+            # return HttpResponse(cwd)
+            # if __name__ == '__main__':
+            theirs = 'hackathon2017.their'
+            # os.mkdir(theirs)
+            # return HttpResponse(cwd)
+            os.system('git reset --hard')
+            subprocess.run(['git', 'clone', r, theirs])
 
-                # #Remove ours requirements file
-                #os.remove('requirements.txt')
-                # Get theirs
-                shutil.copyfile(os.path.join(theirs, 'requirements.txt'),
-                                'requirements.txt')
+            # #Remove ours requirements file
+            # os.remove('requirements.txt')
+            # Get theirs
+            shutil.copyfile(os.path.join(theirs, 'requirements.txt'),
+                            'requirements.txt')
 
-                # Remove our hackathon/solution
-                shutil.rmtree(os.path.join('hackathon', 'solution'))
-                # Get their hackathon/solution
-                shutil.copytree(os.path.join(theirs, 'hackathon', 'solution'),
-                                os.path.join('hackathon', 'solution'))
+            # Remove our hackathon/solution
+            shutil.rmtree(os.path.join('hackathon', 'solution'))
+            # Get their hackathon/solution
+            shutil.copytree(os.path.join(theirs, 'hackathon', 'solution'),
+                            os.path.join('hackathon', 'solution'))
 
-                # Remove their repository completely
-                if sys.platform.startswith('win'):
-                    os.system('rmdir /S /Q "{}"'.format(theirs))
-                else:
-                    shutil.rmtree(theirs)
+            # Remove their repository completely
+            if sys.platform.startswith('win'):
+                os.system('rmdir /S /Q "{}"'.format(theirs))
+            else:
+                shutil.rmtree(theirs)
 
-                subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-                subprocess.run(['python', 'run.py'])
-                return HttpResponse(cwd)
-            # return HttpResponseRedirect('/viz.html')
+            subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+            subprocess.run(['python', 'run.py'])
+            return HttpResponse(cwd)
+        # return HttpResponseRedirect('/viz.html')
+
+
     return render(request, 'submit_sol.html', {"error": error})
+
 
 def visualize(request):
     return render(request, "viz.html")
+
 
 def results(request):
     if request.is_ajax:
@@ -60,7 +66,8 @@ def results(request):
     else:
         return HttpResponse("NO!")
 
+
 def hallOfFame(request):
     entries = HallOfFame.objects.all()
-    context = {"entries":entries}
+    context = {"entries": entries}
     return render(request, "halloffame.html", context)
