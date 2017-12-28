1cb85c0bded3403c937efb9e841772efb209271d
Stefan <stefan.soic@typhoon-hil.ch>
2017-12-28 17:48:26 +0100

    Dodat if name==main u submit view

--- typhoon/views.py
+++ typhoon/views.py
@@ -20,34 +20,34 @@ def submit_sol(request):
         else:
             os.chdir("./hackathon")
             cwd = str(os.getcwd())
-            #if __name__ == '__main__':
-            theirs = 'hackathon2017.their'
-            # os.mkdir(theirs)
-            # return HttpResponse(cwd)
-            os.system('git reset --hard')
-            subprocess.run(['git', 'clone', r, theirs])
+            if __name__ == '__main__':
+                theirs = 'hackathon2017.their'
+                # os.mkdir(theirs)
+                # return HttpResponse(cwd)
+                os.system('git reset --hard')
+                subprocess.run(['git', 'clone', r, theirs])
 
-            # #Remove ours requirements file
-            #os.remove('requirements.txt')
-            # Get theirs
-            shutil.copyfile(os.path.join(theirs, 'requirements.txt'),
-                            'requirements.txt')
+                # #Remove ours requirements file
+                #os.remove('requirements.txt')
+                # Get theirs
+                shutil.copyfile(os.path.join(theirs, 'requirements.txt'),
+                                'requirements.txt')
 
-            # Remove our hackathon/solution
-            shutil.rmtree(os.path.join('hackathon', 'solution'))
-            # Get their hackathon/solution
-            shutil.copytree(os.path.join(theirs, 'hackathon', 'solution'),
-                            os.path.join('hackathon', 'solution'))
+                # Remove our hackathon/solution
+                shutil.rmtree(os.path.join('hackathon', 'solution'))
+                # Get their hackathon/solution
+                shutil.copytree(os.path.join(theirs, 'hackathon', 'solution'),
+                                os.path.join('hackathon', 'solution'))
 
-            # Remove their repository completely
-            if sys.platform.startswith('win'):
-                os.system('rmdir /S /Q "{}"'.format(theirs))
-            else:
-                shutil.rmtree(theirs)
+                # Remove their repository completely
+                if sys.platform.startswith('win'):
+                    os.system('rmdir /S /Q "{}"'.format(theirs))
+                else:
+                    shutil.rmtree(theirs)
 
-            subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-            subprocess.run(['python', 'run.py'])
-            return HttpResponse(cwd)
+                subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+                subprocess.run(['python', 'run.py'])
+                return HttpResponse(cwd)
             # return HttpResponseRedirect('/viz.html')
     return render(request, 'submit_sol.html', {"error": error})
 
